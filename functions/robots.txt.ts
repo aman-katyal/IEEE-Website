3f5994775e1a8ffcc4c8b3baeeace38c70a91aaa
aman-katyal <itsamankatyal@gmail.com>
2026-07-06 19:58:58 -0700

    seo: dynamically resolve sitemap and robots.txt based on current origin
    Deletes static robots.txt and sitemap.xml files from public/ assets folder to allow Cloudflare Pages functions to intercept requests. Adds functions/robots.txt.ts and functions/sitemap.xml.ts serverless edge functions to dynamically render the robots.txt and sitemap.xml with the host's actual current origin (localhost, pages.dev, or custom domains).

---
+++ functions/robots.txt.ts
@@ -0,0 +1,17 @@
+export const onRequest: PagesFunction = async (context) => {
+  const url = new URL(context.request.url);
+  const origin = url.origin;
+
+  const robotsText = `User-agent: *
+Disallow:
+
+Sitemap: ${origin}/sitemap.xml
+`;
+
+  return new Response(robotsText, {
+    headers: {
+      "content-type": "text/plain; charset=utf-8",
+      "cache-control": "public, max-age=3600",
+    },
+  });
+};
--- functions/sitemap.xml.ts
+++ functions/sitemap.xml.ts
@@ -1,88 +1,100 @@
-<?xml version="1.0" encoding="UTF-8"?>
+export const onRequest: PagesFunction = async (context) => {
+  const url = new URL(context.request.url);
+  const origin = url.origin;
+
+  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://purdueieee.org/</loc>
+    <loc>${origin}/</loc>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/about</loc>
+    <loc>${origin}/about</loc>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/committees</loc>
+    <loc>${origin}/committees</loc>
     <changefreq>weekly</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/committee/rov</loc>
+    <loc>${origin}/committee/rov</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/committee/csociety</loc>
+    <loc>${origin}/committee/csociety</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/committee/aesc</loc>
+    <loc>${origin}/committee/aesc</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/committee/software-saturdays</loc>
+    <loc>${origin}/committee/software-saturdays</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/committee/racing</loc>
+    <loc>${origin}/committee/racing</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/committee/mtts</loc>
+    <loc>${origin}/committee/mtts</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/committee/embs</loc>
+    <loc>${origin}/committee/embs</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/committee/eds</loc>
+    <loc>${origin}/committee/eds</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/committee/smc</loc>
+    <loc>${origin}/committee/smc</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/officers</loc>
+    <loc>${origin}/officers</loc>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/calendar</loc>
+    <loc>${origin}/calendar</loc>
     <changefreq>daily</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/join</loc>
+    <loc>${origin}/join</loc>
     <changefreq>monthly</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/partners</loc>
+    <loc>${origin}/partners</loc>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://purdueieee.org/constitution</loc>
+    <loc>${origin}/constitution</loc>
     <changefreq>yearly</changefreq>
     <priority>0.5</priority>
   </url>
-</urlset>
+</urlset>`;
+
+  return new Response(sitemapXml.trim(), {
+    headers: {
+      "content-type": "application/xml; charset=utf-8",
+      "cache-control": "public, max-age=3600",
+    },
+  });
+};
